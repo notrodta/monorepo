9f46d65f70b415a21c6b311fcee58ef98d74ab51
Rod <rody101@gmail.com>
2024-06-23 00:14:15 -0400

    created http proxy server

--- apps/my-express-api/src/main.ts
+++ apps/my-express-api/src/main.ts
@@ -5,17 +5,48 @@
 
 import express from 'express';
 import * as path from 'path';
+import http from 'http';
+import httpProxy from 'http-proxy'
 
-const app = express();
+// const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to my-express-api!' });
-});
+// app.get('/api', (req, res) => {
+//   res.send({ message: 'Welcome to my-express-api!' });
+// });
 
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+// const port = process.env.PORT || 3333;
+// const server = app.listen(port, () => {
+//   console.log(`Listening at http://localhost:${port}/api`);
+// });
+// server.on('error', console.error);
+
+//
+// Create a proxy server with latency
+//
+var proxy = httpProxy.createProxyServer();
+ 
+//
+// Create your server that makes an operation that waits a while
+// and then proxies the request
+//
+http.createServer(function (req, res) {
+  // This simulates an operation that takes 500ms to execute
+  setTimeout(function () {
+    proxy.web(req, res, {
+      target: 'http://localhost:9008'
+    });
+  }, 1000);
+}).listen(8008);
+ 
+//
+
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+  res.write('request successfully proxied to: ' + '9008' + '...3');
+  res.end();
+}).listen(9008);
